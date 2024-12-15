e6638c135fa9cbb23bbf12b83c0b0e2e22e94359
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-12-15 12:20:47 +0500

    Add userRoutes to server + Use express middlewares

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
 const port = process.env.PORT;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/users", userRoutes);
+
 app.listen(port, () => {
-    console.log("Server started at port: " + port);
+    console.log(`Server started at port: ${port}`);
 });
