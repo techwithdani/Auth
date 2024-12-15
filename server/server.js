5da3e3f83fc4cdbc785631db48f41a7f25bc5bbd
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-12-15 12:55:30 +0500

    Add custom error handler middleware to server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
 
 dotenv.config();
 
@@ -11,6 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server started at port: ${port}`);
