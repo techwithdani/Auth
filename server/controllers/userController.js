f739fddfbfa79dac96e8361fbcc659cc53e9806e
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-12-18 17:15:04 +0500

    Create updateUserProfile controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -16,7 +16,27 @@ const getUserProfile = asyncHandler(async (req, res) => {
 });
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.json({ message: "User Profile Updated" });
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error("User Not Found");
+    }
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
