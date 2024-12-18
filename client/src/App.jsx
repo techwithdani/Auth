aafd359ceaa3c4bc42ffdb12866184350a2d819b
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-12-19 00:34:14 +0500

    Create navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <>
-      <h1>ByteBridge</h1>
+      <NavBar />
     </>
   );
 }
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <a className="btn btn-ghost text-xl">ByteBridge</a>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
