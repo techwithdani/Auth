310619c4a6a65156936ffc5d61c13421fa762b10
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-12-19 00:41:08 +0500

    Create footer component + Change navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
+import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <>
       <NavBar />
+      <SignUp />
+      <Footer />
     </>
   );
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,8 @@
 const NavBar = () => {
   return (
     <>
-      <div className="navbar bg-base-100">
-        <a className="btn btn-ghost text-xl">ByteBridge</a>
+      <div className="navbar bg-base-300">
+        <button className="btn btn-ghost text-xl">ByteBridge</button>
       </div>
     </>
   );
