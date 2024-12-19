9418162ebfba0cabdb07279bdaab061ddee0a853
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-12-19 17:32:52 +0500

    Create react router + Change font size of hero

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Welcome to ByteBridge</h1>
+            <h1 className="text-4xl font-bold">Welcome to ByteBridge</h1>
             <p className="py-6">Connecting Conversations Seamlessly</p>
             <button className="btn btn-outline btn-primary">
               Join the Conversation
