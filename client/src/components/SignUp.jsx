4617b9ecdee1c3315073818d99ea17883224e430
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-12-20 20:16:41 +0500

    Add state to signup component + Create submitForm arrow function

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    console.log("Submit");
+  };
+
   return (
     <>
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form className="card-body">
+            <form className="card-body" onSubmit={submitForm}>
               <div className="form-control">
                 <label className="input input-bordered flex items-center gap-2">
                   <svg
@@ -17,7 +28,13 @@ const Signup = () => {
                   >
                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
                   </svg>
-                  <input type="text" className="grow" placeholder="Name" />
+                  <input
+                    type="text"
+                    className="grow"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
                 </label>
               </div>
               <div className="form-control">
@@ -31,7 +48,13 @@ const Signup = () => {
                     <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
                     <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
                   </svg>
-                  <input type="text" className="grow" placeholder="Email" />
+                  <input
+                    type="text"
+                    className="grow"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </label>
               </div>
               <div className="form-control">
@@ -52,6 +75,8 @@ const Signup = () => {
                     type="password"
                     className="grow"
                     placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                   />
                 </label>
               </div>
@@ -73,6 +98,8 @@ const Signup = () => {
                     type="password"
                     className="grow"
                     placeholder="Confirm Password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
                   />
                 </label>
                 <label className="label">
