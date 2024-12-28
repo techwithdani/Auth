1f1339328b5203457163dc7ff6a60f07b38c0277
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-12-28 12:52:43 +0500

    Create home page

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import MainLayout from "../layouts/MainLayout";
+
+const HomePage = () => {
+  return (
+    <>
+      <MainLayout>
+        <h1>Home Page</h1>
+      </MainLayout>
+    </>
+  );
+};
+
+export default HomePage;
