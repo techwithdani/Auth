5bae7f8f7563af6332481ee9e5b249c2eadbbcec
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-12-29 17:48:09 +0500

    Create user profile page

---
+++ client/src/pages/ProfilePage.jsx
@@ -0,0 +1,14 @@
+import Profile from "../components/Profile";
+import MainLayout from "../layouts/MainLayout";
+
+const ProfilePage = () => {
+  return (
+    <>
+      <MainLayout>
+        <Profile />
+      </MainLayout>
+    </>
+  );
+};
+
+export default ProfilePage;
