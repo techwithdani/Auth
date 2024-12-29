4c43397e0751d3c67748efe999563af0db14ed46
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-12-29 20:00:18 +0500

    Create user profile card to display user information

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,7 +1,33 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 const Profile = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+
   return (
     <>
-      <h1>User Profile</h1>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="card glass w-96">
+          <figure>
+            <div className="avatar">
+              <div className="w-24 rounded mt-6">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Name</h2>
+            <p>{userInfo.name}</p>
+            <h2 className="card-title">Email</h2>
+            <p>{userInfo.email}</p>
+            <div className="card-actions justify-end">
+              <Link to={"/update-profile"}>
+                <button className="btn btn-primary">Update Profile</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
