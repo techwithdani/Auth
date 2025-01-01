23c4215b44bef1a63d55e3a3723d3a77c1544e26
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-01-01 20:20:14 +0500

    Exclude password in response for security reasons

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -16,7 +16,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
 });
 
 const getAllUsers = asyncHandler(async (req, res) => {
-  const users = await User.find();
+  const users = await User.find().select("-password");
 
   if (users) {
     res.status(200).json(users);
