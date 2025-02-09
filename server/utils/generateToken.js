b6e60c79bb098bdfa4b847c048f701c549deff99
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-02-09 20:04:05 +0500

    Refactor

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -2,14 +2,14 @@ const jwt = require("jsonwebtoken");
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "2d",
+    expiresIn: "1d",
   });
 
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
     sameSite: "strict",
-    maxAge: 2 * 24 * 60 * 60 * 1000,
+    maxAge: 1 * 24 * 60 * 60 * 1000,
   });
 };
 
